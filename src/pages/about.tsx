b460b795b4187159c6a3b277f24113ae7b4b68df
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-01-01 23:17:05 +0900

    update: about, README の更新

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -12,11 +12,19 @@ const About: React.FC = () => {
                         <div className='grid gap-8 md:grid-cols-2 lg:gap-12'>
                             <div>
                                 <div className='h-128 overflow-hidden rounded-lg bg-gray'>
-                                    <Image src='/images/robot_and_hogeta.jpeg' width={600} height={1000} alt='走行体とホゲータ' />
+                                    <Image className='h-full w-full object-cover object-center'
+                                        src='/images/robot_and_hogeta.jpeg'
+                                        width={600} height={1000}
+                                        alt='走行体とホゲータ' />
                                 </div>
                             </div>
-                            <h1 className='text-center text-2xl font-bold'>About Me</h1>
-                            <p className='md:text-center'>宮崎大学所属の木村優哉です！</p>
+                            <div>
+                                <h1 className='text-center text-2xl font-bold'>About Me</h1>
+                                <p className='md:text-center'>
+                                    宮崎大学の大学院に在籍している木村優哉です！
+                                    プログラミングを専攻する学科に所属しており、研究やETロボコン、Webアプリケーション開発のアルバイトなどを行っております！
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
