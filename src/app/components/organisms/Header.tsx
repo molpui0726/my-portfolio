aae15ca456acddc6c52905109bc8a0430bdb0731
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-01-02 16:04:14 +0900

    update: メディアオブジェクトをコンポーネント化

--- src/app/components/organisms/Header.tsx
+++ src/app/components/organisms/Header.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import Link from 'next/link';
-import NavButton from '@/components/atoms/button/NavButton';
+import React from "react";
+import Link from "next/link";
+import NavButton from "@/components/atoms/button/NavButton";
 
 const Header = () => {
-  return (
-    <div className='bg-black text-white p-4'>
-      <nav className='flex gap-4'>
-        <NavButton href="../">Home</NavButton>
-        <NavButton href="/about">About</NavButton>
-        <NavButton href="/contact">Contact</NavButton>
-      </nav>
-    </div>
-  );
+    return (
+        <div className="bg-black text-white p-4">
+            <nav className="flex gap-4">
+                <NavButton href="../">Home</NavButton>
+                <NavButton href="/about">About</NavButton>
+                <NavButton href="/contact">Contact</NavButton>
+            </nav>
+        </div>
+    );
 };
 
 export default Header;
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,33 +1,20 @@
-import React from 'react';
-import Layout from '@/app/Layout';
-import Image from 'next/image';
-import Link from 'next/link';
+import React from "react";
+import Layout from "@/app/Layout";
+import MediaObject from "@/app/components/organisms/MediaObject";
+import Image from "next/image";
+import Link from "next/link";
 
 const About: React.FC = () => {
     return (
         <div>
             <Layout>
-                <div className='bg-white py-6 sm:py-8 lg:py-12'>
-                    <div className='mx-auto'>
-                        <div className='grid gap-8 md:grid-cols-2 lg:gap-12'>
-                            <div>
-                                <div className='h-128 overflow-hidden rounded-lg bg-gray'>
-                                    <Image className='h-full w-full object-cover object-center'
-                                        src='/images/robot_and_hogeta.jpeg'
-                                        width={600} height={1000}
-                                        alt='走行体とホゲータ' />
-                                </div>
-                            </div>
-                            <div>
-                                <h1 className='text-center text-2xl font-bold'>About Me</h1>
-                                <p className='md:text-center'>
-                                    宮崎大学の大学院に在籍している木村優哉です！
-                                    プログラミングを専攻する学科に所属しており、研究やETロボコン、Webアプリケーション開発のアルバイトなどを行っております！
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <MediaObject
+                    src="/images/robot_and_hogeta.jpeg"
+                    alt="走行体とホゲータ"
+                    heading="About Me"
+                    text="宮崎大学の大学院に在籍している木村優哉です！
+                    プログラミングを専攻する学科に所属しており、研究やETロボコン、Webアプリケーション開発のアルバイトなどを行っております！"
+                />
                 <p>作成中</p>
                 <Link href="/">Go back to Home</Link>
             </Layout>
