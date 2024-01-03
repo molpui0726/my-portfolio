ca28f5c1f0c06cdae564b1f34d63e402233e0e5f
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-01-04 00:41:21 +0900

    update: @のエイリアスを適用

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
 
 const config: StorybookConfig = {
     stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
@@ -40,6 +41,15 @@ const config: StorybookConfig = {
     },
     docs: {
         autodocs: "tag",
+    },
+    webpackFinal: async (config) => {
+        if (config.resolve) {
+            config.resolve.alias = {
+                ...config.resolve.alias,
+                "@": path.resolve(__dirname, "../src/app"),
+            };
+        }
+        return config;
     },
 };
 export default config;
