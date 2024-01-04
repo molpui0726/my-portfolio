e7b10cbbaec2dc68b1be521811f14819cda91d9e
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-01-05 07:31:18 +0900

    fix: stories ファイルの import 文を修正

--- src/app/components/organisms/Header.stories.tsx
+++ src/app/components/organisms/Header.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
-import * as NavButtonStories from "@/components/atoms/button/NavButton.stories.tsx";
+import * as NavButtonStories from "@/components/atoms/button/NavButton.stories";
 import Header from "./Header";
 
 const meta = {
