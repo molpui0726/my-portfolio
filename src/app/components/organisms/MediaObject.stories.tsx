39ab0e4374bfe17433eb20ba261903dc224c01d5
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-05-07 20:39:08 +0900

    update: MediaObjectのstoriesファイルを作成

---
+++ src/app/components/organisms/MediaObject.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import MediaObject from "./MediaObject";
+
+const meta = {
+    title: "components/organisms/MediaObject",
+    component: MediaObject,
+} satisfies Meta<typeof MediaObject>;
+
+export default meta;
+
+type Story = StoryObj<typeof MediaObject>;
+
+export const Default: Story = {
+    args: {
+        src: '/images/robot_and_hogeta.jpeg',
+        alt: 'MediaObject',
+        heading: 'Sample Heading',
+        text: 'Sample Text'
+    },
+    render: (args) => <MediaObject {...args}></MediaObject>,
+};
