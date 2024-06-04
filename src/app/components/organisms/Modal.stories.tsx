9aac5cb26da86d48edf4249adb53aac7fcc61276
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-06-04 18:34:29 +0900

    update: Modalのstoriesファイルの書式を一部変更

--- src/app/components/organisms/Modal.stories.tsx
+++ src/app/components/organisms/Modal.stories.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
+import { withActions } from '@storybook/addon-actions/decorator';
 import { Modal } from "./Modal";
 
 const meta = {
     title: "components/organisms/Modal",
     component: Modal,
+    decorators: [withActions],
 } satisfies Meta<typeof Modal>
+
 export default meta;
 
 type Story = StoryObj<typeof Modal>;
 
 export const Default: Story = {
     args: {
-        open: true,
+        isOpen: true,
         title: 'Sample Title',
         src: '/images/robot_and_hogeta.jpeg',
         alt: 'Modal',
-        text: 'Sample Text'
+        text: 'Sample Text',
     },
 };
