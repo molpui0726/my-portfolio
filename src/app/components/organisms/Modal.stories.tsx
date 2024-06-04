adffe3332c0e288dec2e368df40062ff7cf3fa1b
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-06-04 20:19:46 +0900

    add: Modalを閉じるボタンのテストをstoriesファイルに追加！

--- src/app/components/organisms/Modal.stories.tsx
+++ src/app/components/organisms/Modal.stories.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
+import { expect, fn, userEvent, within } from '@storybook/test';
 import { withActions } from '@storybook/addon-actions/decorator';
 import { Modal } from "./Modal";
 
@@ -7,6 +8,9 @@ const meta = {
     title: "components/organisms/Modal",
     component: Modal,
     decorators: [withActions],
+    args: {
+        closeModal: fn(),
+    }
 } satisfies Meta<typeof Modal>
 
 export default meta;
@@ -20,5 +24,24 @@ export const Default: Story = {
         alt: 'Modal',
         modalTitle: 'Sample Title',
         modalText: 'Sample Text',
+        closeModal: fn(),
     },
 };
+
+export const CloseModal: Story = {
+    args: {
+        isModalOpen: true,
+        src: '/images/robot_and_hogeta.jpeg',
+        alt: 'Modal',
+        modalTitle: 'Sample Title',
+        modalText: 'Sample Text',
+        closeModal: fn(),
+    },
+    play: async ({ args, canvasElement }) => {
+        const canvas = within(canvasElement);
+        const closeButton = canvas.getByRole('button');
+        await expect(closeButton).toBeInTheDocument();
+        await userEvent.click(closeButton);
+        await expect(closeButton).toBeInTheDocument();
+    }
+}
