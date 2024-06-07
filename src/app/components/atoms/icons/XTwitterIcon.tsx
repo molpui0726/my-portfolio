7563373e3c645c79705adc508f08b965cfabd66d
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-06-07 22:41:05 +0900

    refactor: 不要なコメントの削除

--- src/app/components/atoms/icons/XTwitterIcon.tsx
+++ src/app/components/atoms/icons/XTwitterIcon.tsx
@@ -6,7 +6,6 @@ export const XTwitterIcon: React.FC = () => {
 	return (
 		<Link href='https://x.com/Hogeta_KL'>
 			<div className='w-20 h-20'>
-				{/* <Image src='/images/icons/logo.png' alt='XTwitterIcon' width={200} height={200} /> */}
 				<XTwitterSVG width={200} height={200} />
 			</div>
 		</Link>
