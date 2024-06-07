195e8f160b83e3d482de98ddc85be9e21e4884a0
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-06-07 19:59:25 +0900

    fix: Xのアイコン画像をSVGに変更

--- src/app/components/atoms/icons/XTwitterIcon.tsx
+++ src/app/components/atoms/icons/XTwitterIcon.tsx
@@ -1,16 +1,13 @@
+import type React from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
+import XTwitterSVG from 'public/images/icons/XTwitter-logo.svg';
 
 export const XTwitterIcon: React.FC = () => {
 	return (
 		<Link href='https://x.com/Hogeta_KL'>
 			<div className='w-20 h-20'>
-				<Image
-					src='/images/icons/XTwitter-logo-black.png'
-					alt='XTwitterIcon'
-					width={200}
-					height={200}
-				/>
+				{/* <Image src='/images/icons/logo.png' alt='XTwitterIcon' width={200} height={200} /> */}
+				<XTwitterSVG width={200} height={200} />
 			</div>
 		</Link>
 	);
