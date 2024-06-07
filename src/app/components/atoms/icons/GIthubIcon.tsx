be748b41723a274a08716017995a85c06b1112a6
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-06-07 20:38:37 +0900

    fix: Githubアイコン画像とコンポーネントをSVGに変更

--- src/app/components/atoms/icons/GIthubIcon.tsx
+++ src/app/components/atoms/icons/GIthubIcon.tsx
@@ -1,17 +1,12 @@
+import type React from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
+import GithubLogo from 'public/images/icons/GithubLogo.svg';
 
 export const GithubIcon: React.FC = () => {
 	return (
 		<Link href='https://github.com/YKhm20020'>
-			<div className='rounded-full w-20 h-20'>
-				<Image
-					src='/images/icons/github-mark.png'
-					alt='GithubIcon'
-					width={200}
-					height={200}
-					style={{ borderRadius: '50%' }}
-				/>
+			<div className='rounded-md w-20 h-20 hover:bg-lime-500'>
+				<GithubLogo width={200} height={200} />
 			</div>
 		</Link>
 	);
