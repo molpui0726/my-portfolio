643ee3be1d4b2e04f02265e756963d3ba3252514
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-06-16 20:47:01 +0900

    add: 追加した入力フィールドのstories.tsxファイルの追加

---
+++ src/app/components/atoms/InputField.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { expect, fn, userEvent, within } from '@storybook/test';
+import { withActions } from '@storybook/addon-actions/decorator';
+import { MemolizedInputField as InputField } from './InputField';
+
+const meta = {
+	title: 'components/atoms/InputField',
+	component: InputField,
+	decorators: [withActions],
+} satisfies Meta<typeof InputField>;
+
+export default meta;
+
+type Story = StoryObj<typeof InputField>;
+
+export const Default: Story = {
+	args: {
+		input: 'sample input',
+		hint: 'sample hint',
+	},
+};
