f9a23d99905a4b38f596337be98edc1ed97c9804
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-01 18:51:19 +0900

    fix: 前コミットに適応するようstoriesファイルのimport文や引数を修正

--- src/app/components/atoms/InputField.stories.tsx
+++ src/app/components/atoms/InputField.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { expect, fn, userEvent, within } from '@storybook/test';
 import { withActions } from '@storybook/addon-actions/decorator';
-import { MemolizedInputField as InputField } from './InputField';
+import { InputField } from './InputField';
 
 const meta = {
 	title: 'components/atoms/InputField',
@@ -16,14 +16,16 @@ type Story = StoryObj<typeof InputField>;
 
 export const Default: Story = {
 	args: {
-		input: '',
+		label: 'label',
+		initialValue: '',
 		hint: '',
 	},
 };
 
 export const InputText: Story = {
 	args: {
-		input: 'sample input',
+		label: 'sample label',
+		initialValue: 'sample initial input',
 		hint: 'sample hint',
 	},
 };
