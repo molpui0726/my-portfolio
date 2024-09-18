eba1d305256a0f72cc5ccd63898b190d9a0d24f0
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-18 20:06:24 +0900

    update: 動作を伴う変数名を修正するため、closeXX を onCloseXXに変更

--- src/app/components/molecules/card/WorkCard.tsx
+++ src/app/components/molecules/card/WorkCard.tsx
@@ -1,59 +1,70 @@
-import React, { useState } from "react";
-import Image from "next/image";
-import { Modal } from "@/app/components/organisms/Modal"
+import type React from 'react';
+import { useState } from 'react';
+import Image from 'next/image';
+import { Modal } from '@/app/components/organisms/Modal';
 
 type WorkCardProps = {
-    src: string;
-    alt?: string;
-    workTitle: string;
-    techs: string[];
-    modalText: string; // モーダル画面の説明文
-}
+	src: string;
+	alt?: string;
+	workTitle: string;
+	techs: string[];
+	modalText: string; // モーダル画面の説明文
+};
 
 export const WorkCard: React.FC<WorkCardProps> = ({ src, alt, workTitle, techs, modalText }) => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const openModal = () => {
-        setIsModalOpen(true);
-    };
+	const onOpenModal = () => {
+		setIsModalOpen(true);
+	};
 
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
+	const onCloseModal = () => {
+		setIsModalOpen(false);
+	};
 
-    return (
-        <>
-            <div className="flex justify-center w-full">
-                <div className="relative group flex flex-col shadow-2xl rounded-xl overflow-hidden md:w-80 md:-ml-16 cursor-pointer" onClick={openModal}>
-                    <Image className="object-cover object-center w-full rounded-t-xl lg:h-60 md:h-48"
-                        src={src}
-                        alt={alt ?? workTitle}
-                        width={600}
-                        height={600} />
-                    <div className="bg-white w-full h-full transition duration-500 ease-in-out delay-150 group-hover:translate-y-[-100px]">
-                        <div className="px-6 mt-8">
-                            <h1 className="mt-4 mb-4 text-2xl font-semibold text-neutral-600">{workTitle}</h1>
-                            <div className="overflow-hidden">
-                                <div className="h-20 opacity-0 group-hover:opacity-100 transition-opacity duration-500 delay-300">
-                                    {techs.map((item, index) => (
-                                        <p key={index} className="text-base font-normal text-gray-500 leading-relax">
-                                            {item}
-                                        </p>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <Modal
-                isModalOpen={isModalOpen}
-                src={src}
-                alt={alt ?? workTitle}
-                modalTitle={workTitle}
-                modalText={modalText}
-                closeModal={closeModal}
-            />
-        </>
-    );
+	return (
+		<>
+			<div className='flex justify-center w-full'>
+				<div
+					className='relative group flex flex-col shadow-2xl rounded-xl overflow-hidden md:w-80 md:-ml-16 cursor-pointer'
+					onClick={onOpenModal}
+				>
+					<Image
+						className='object-cover object-center w-full rounded-t-xl lg:h-60 md:h-48'
+						src={src}
+						alt={alt ?? workTitle}
+						width={600}
+						height={600}
+					/>
+					<div className='bg-white w-full h-full transition duration-500 ease-in-out delay-150 group-hover:translate-y-[-100px]'>
+						<div className='px-6 mt-8'>
+							<h1 className='mt-4 mb-4 text-2xl font-semibold text-neutral-600'>
+								{workTitle}
+							</h1>
+							<div className='overflow-hidden'>
+								<div className='h-20 opacity-0 group-hover:opacity-100 transition-opacity duration-500 delay-300'>
+									{techs.map((item, index) => (
+										<p
+											key={index}
+											className='text-base font-normal text-gray-500 leading-relax'
+										>
+											{item}
+										</p>
+									))}
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<Modal
+				isOpen={isModalOpen}
+				src={src}
+				alt={alt ?? workTitle}
+				modalTitle={workTitle}
+				modalText={modalText}
+				onClose={onCloseModal}
+			/>
+		</>
+	);
 };
