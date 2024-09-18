e8b81f5f53e72329ba36d493069d09fe2a8c7cad
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-18 20:40:03 +0900

    fix: Vercelのビルドに通らない問題を解決するため、元ファイルで変更した変数についてstoriesファイルにも変更を反映

--- src/app/components/molecules/card/WorkCard.stories.tsx
+++ src/app/components/molecules/card/WorkCard.stories.tsx
@@ -1,17 +1,16 @@
-import React from "react";
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from '@storybook/react';
 import { withActions } from '@storybook/addon-actions/decorator';
-import { WorkCard } from "./WorkCard";
+import { WorkCard } from './WorkCard';
 
 const meta = {
-    title: "components/molecules/card/WordCard",
-    component: WorkCard,
-    parameters: {
-        actions: {
-            handles: ["mouseover"],
-        },
-    },
-    decorators: [withActions],
+	title: 'components/molecules/card/WordCard',
+	component: WorkCard,
+	parameters: {
+		actions: {
+			handles: ['mouseover'],
+		},
+	},
+	decorators: [withActions],
 } satisfies Meta<typeof WorkCard>;
 
 export default meta;
@@ -19,10 +18,10 @@ export default meta;
 type Story = StoryObj<typeof WorkCard>;
 
 export const Default: Story = {
-    args: {
-        src: "/images/robot_and_hogeta.jpeg",
-        alt: "sample-alt",
-        workTitle: "workTitle",
-        techs: ["tech1", "tech2", "tech3"],
-    },
+	args: {
+		src: '/images/robot_and_hogeta.jpeg',
+		alt: 'sample-alt',
+		workTitle: 'workTitle',
+		techs: ['tech1', 'tech2', 'tech3'],
+	},
 };
--- src/app/components/organisms/Modal.stories.tsx
+++ src/app/components/organisms/Modal.stories.tsx
@@ -1,47 +1,46 @@
-import React from "react";
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from '@storybook/react';
 import { expect, fn, userEvent, within } from '@storybook/test';
 import { withActions } from '@storybook/addon-actions/decorator';
-import { Modal } from "./Modal";
+import { Modal } from './Modal';
 
 const meta = {
-    title: "components/organisms/Modal",
-    component: Modal,
-    decorators: [withActions],
-    args: {
-        closeModal: fn(),
-    }
-} satisfies Meta<typeof Modal>
+	title: 'components/organisms/Modal',
+	component: Modal,
+	decorators: [withActions],
+	args: {
+		onClose: fn(),
+	},
+} satisfies Meta<typeof Modal>;
 
 export default meta;
 
 type Story = StoryObj<typeof Modal>;
 
 export const Default: Story = {
-    args: {
-        isModalOpen: true,
-        src: '/images/robot_and_hogeta.jpeg',
-        alt: 'Modal',
-        modalTitle: 'Sample Title',
-        modalText: 'Sample Text',
-        closeModal: fn(),
-    },
+	args: {
+		isOpen: true,
+		src: '/images/robot_and_hogeta.jpeg',
+		alt: 'Modal',
+		modalTitle: 'Sample Title',
+		modalText: 'Sample Text',
+		onClose: fn(),
+	},
 };
 
 export const CloseModal: Story = {
-    args: {
-        isModalOpen: true,
-        src: '/images/robot_and_hogeta.jpeg',
-        alt: 'Modal',
-        modalTitle: 'Sample Title',
-        modalText: 'Sample Text',
-        closeModal: fn(),
-    },
-    play: async ({ args, canvasElement }) => {
-        const canvas = within(canvasElement);
-        const closeButton = canvas.getByRole('button');
-        await expect(closeButton).toBeInTheDocument();
-        await userEvent.click(closeButton);
-        await expect(closeButton).toBeInTheDocument();
-    }
-}
+	args: {
+		isOpen: true,
+		src: '/images/robot_and_hogeta.jpeg',
+		alt: 'Modal',
+		modalTitle: 'Sample Title',
+		modalText: 'Sample Text',
+		onClose: fn(),
+	},
+	play: async ({ args, canvasElement }) => {
+		const canvas = within(canvasElement);
+		const closeButton = canvas.getByRole('button');
+		await expect(closeButton).toBeInTheDocument();
+		await userEvent.click(closeButton);
+		await expect(closeButton).toBeInTheDocument();
+	},
+};
