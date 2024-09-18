32f32a7355f155cc651f5b5f58ba559b5082cc9d
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-18 14:58:35 +0900

    update: ホバー時のテスト追加のため、ホバーしたかを判別するクラスを追加

--- src/app/components/atoms/button/NavButton.tsx
+++ src/app/components/atoms/button/NavButton.tsx
@@ -1,30 +1,31 @@
-import React, { ReactNode, useState, useEffect } from 'react';
+import type React from 'react';
+import { type ReactNode, useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
 type NavButtonProps = {
-    href: string;
-    children: ReactNode;
-}
+	href: string;
+	children: ReactNode;
+};
 
 const NavButton: React.FC<NavButtonProps> = ({ href, children }: NavButtonProps) => {
+	const router = useRouter();
+	const isActive = router.pathname === href;
 
-    const router = useRouter();
-    const isActive = router.pathname === href;
+	const [isHovered, setIsHovered] = useState(false);
 
-    const [isHovered, setIsHovered] = useState(false);
-
-    return (
-        <Link href={href}
-            className={`bg-neutral-950 text-stone-300 text-base px-2 py-1 rounded-full text-transition md:duration-700 ease-in-out
-                    ${isActive ? "!bg-white text-stone-600 text-lg" : ""}
-                    ${isHovered ? "hover:bg-white hover:text-black hover:text-lg" : ""}`}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-        >
-            {children}
-        </Link>
-    );
+	return (
+		<Link
+			href={href}
+			className={`bg-neutral-950 text-stone-300 text-base px-2 py-1 rounded-full text-transition md:duration-700 ease-in-out
+                    ${isActive ? '!bg-white text-stone-600 text-lg' : ''}
+                    ${isHovered ? 'hover:bg-white hover:text-black hover:text-lg link-hover' : ''}`}
+			onMouseEnter={() => setIsHovered(true)}
+			onMouseLeave={() => setIsHovered(false)}
+		>
+			{children}
+		</Link>
+	);
 };
 
 export default NavButton;
