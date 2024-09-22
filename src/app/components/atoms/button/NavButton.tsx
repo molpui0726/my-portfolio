5075fb15a59bfc14b697557eecb362665ce83795
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-23 01:03:18 +0900

    update: ホバー時の状態変数がCSS以外に必要ないので、削除してhover:で全て管理

--- src/app/components/atoms/button/NavButton.tsx
+++ src/app/components/atoms/button/NavButton.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import type React from 'react';
-import { useState } from 'react';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
@@ -14,16 +13,12 @@ const NavButton: React.FC<NavButtonProps> = ({ href, children }: NavButtonProps)
 	const pathname = usePathname();
 	const isActive = pathname === href;
 
-	const [isHovered, setIsHovered] = useState(false);
-
 	return (
 		<Link
 			href={href}
 			className={`bg-neutral-950 text-stone-300 text-base px-2 py-1 rounded-full text-transition md:duration-700 ease-in-out
-                    ${isActive ? '!bg-white text-stone-600 text-lg' : ''}
-                    ${isHovered ? 'hover:bg-white hover:text-black hover:text-lg link-hover' : ''}`}
-			onMouseEnter={() => setIsHovered(true)}
-			onMouseLeave={() => setIsHovered(false)}
+                        hover:bg-white hover:text-black hover:text-lg link-hover
+                        ${isActive ? '!bg-white text-stone-600 text-lg' : ''}`}
 		>
 			{children}
 		</Link>
