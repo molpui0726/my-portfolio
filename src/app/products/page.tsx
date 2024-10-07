97b11ea701111ec05df851b32dbedf00b9527096
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-10-07 17:23:11 +0900

    add: 成果物を紹介する Products ページを仮作成

---
+++ src/app/products/page.tsx
@@ -0,0 +1,18 @@
+import type React from 'react';
+import { MediaObject } from '../components/organisms/MediaObject';
+
+const Products: React.FC = () => {
+	return (
+		<div className='bg-white flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-12'>
+			<MediaObject
+				src='/images/robot_and_hogeta.jpeg'
+				alt='走行体とホゲータ'
+				heading='Products'
+                text='成果物について紹介するページです。Webアプリケーション開発のアルバイトや、産学連携の研究など、
+                一部詳細を非公開としているものもあります。組み込み開発からサーバー構築やAWS, Google Cloud など、色々やってます。'
+			/>
+		</div>
+	);
+};
+
+export default Products
