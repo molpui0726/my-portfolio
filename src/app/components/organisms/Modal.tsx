8d71a266a2e94f44f4c207b08f3090cd0060e78b
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-10-07 20:04:06 +0900

    fix: md 以上でモーダルの画面で圧迫感があったため、左右にモーダルのパディングを追加

--- src/app/components/organisms/Modal.tsx
+++ src/app/components/organisms/Modal.tsx
@@ -56,7 +56,7 @@ export const Modal: React.FC<ModalProps> = ({
 	}
 
 	return (
-		<div className='fixed inset-0 z-50 flex items-center justify-center'>
+		<div className='fixed inset-0 z-50 flex items-center justify-center　px-4 sm:px-6 lg:px-16 py-12'>
 			<div
 				className='fixed inset-0 bg-black bg-opacity-50'
 				onClick={onClose}
