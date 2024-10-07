0fcf2ae5a842180447dff711bd53e306a6d593b6
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-10-07 20:01:55 +0900

    update: エンジニアバイトのカードコンポーネントを追加し、並び方を調整

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -12,7 +12,7 @@ const Products: React.FC = () => {
 				text='成果物について紹介するページです。Webアプリケーション開発のアルバイトや、産学連携の研究など、
                 一部詳細を非公開としているものもあります。組み込み開発からサーバー構築やAWS, Google Cloud など、色々やってます。'
 			/>
-			<div>
+			<div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 md:grid-rows-2 lg:gap-x-6 gap-y-8 place-items-center justify-center items-center'>
 				<WorkCard
 					src='/images/robocon.png'
 					alt='走行帯が走っている写真とモデルの画像'
@@ -22,7 +22,16 @@ const Products: React.FC = () => {
                     リアルコース上を機械に走らせて点数を競う走行部門と、
                     設計書の出来を評価するモデル部門があります。
                     2023年は九州地区大会で総合優勝、全国大会で総合6位の成績を収めました。
-                    2024年はチームリーダーを務め、九州地区大会で総合準優勝、モデル優勝の成績を収めました'
+                    2024年はチームリーダーを務め、九州地区大会で総合準優勝、モデル優勝の成績を収めました。
+                    10人ほどで半年間開発を続け、コード量はテスト含めて9000行から10000行ほどとなります。'
+				/>
+				<WorkCard
+					src='/images/part_time_job.png'
+					alt='Webアプリケーションのアルバイトでの技術スタック'
+					workTitle='エンジニアバイト'
+					techs={['TypeScript', 'Tailwindcss', 'Storybook']}
+					modalText='Webアプリケーション開発のアルバイトに1年強従事。
+                    主にフロントを担当しており、現在は AWS, Google Cloud も利用。'
 				/>
 			</div>
 		</div>
