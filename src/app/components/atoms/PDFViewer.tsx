a05e825cb48c83faa031ba51bc225d052cefabea
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-11-06 12:42:52 +0900

    update: react-pdf が tailwindcss によるレスポンシブデザインが実装できなかったため、Javascript で実装

--- src/app/components/atoms/PDFViewer.tsx
+++ src/app/components/atoms/PDFViewer.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import type React from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 
@@ -15,21 +15,48 @@ type PDFViewerProps = {
 
 export const PDFViewer: React.FC<PDFViewerProps> = ({ filePath }: PDFViewerProps) => {
 	const [numPages, setNumPages] = useState(1);
+	const [pageWidth, setPageWidth] = useState(500); // PDFページの初期幅
 	const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
 		setNumPages(numPages);
 	};
 
+	useEffect(() => {
+		// PDFページの大きさを更新する関数
+		const updatePageWidth = () => {
+			const screenWidth = window.innerWidth;
+			if (screenWidth < 640) {
+				setPageWidth(screenWidth * 0.9); // スマホサイズ
+			} else if (screenWidth < 1024) {
+				setPageWidth(700); // タブレットサイズ
+			} else {
+				setPageWidth(1000); // デスクトップサイズ
+			}
+		};
+
+		// 初回設定とリサイズイベントリスナー
+		updatePageWidth();
+		window.addEventListener('resize', updatePageWidth);
+		return () => window.removeEventListener('resize', updatePageWidth);
+	}, []);
+
 	return (
-		<Document file={filePath} onLoadSuccess={onDocumentLoadSuccess}>
-			{Array.from({ length: numPages }, (_, index) => (
-				<Page
-					key={`page_${index + 1}`}
-					pageNumber={index + 1}
-					renderAnnotationLayer={false}
-					renderTextLayer={false}
-					width={500}
-				/>
-			))}
-		</Document>
+		<>
+			<div className='flex justify-center'>
+				<Document file={filePath} onLoadSuccess={onDocumentLoadSuccess}>
+					<div className='flex flex-col items-center gap-8'>
+						{Array.from({ length: numPages }, (_, index) => (
+							<Page
+								key={`page_${index + 1}`}
+								pageNumber={index + 1}
+								renderAnnotationLayer={false}
+								renderTextLayer={false}
+								width={pageWidth}
+								className='mb-4 shadow-lg'
+							/>
+						))}
+					</div>
+				</Document>
+			</div>
+		</>
 	);
 };
