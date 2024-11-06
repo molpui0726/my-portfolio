5c1bf42515fae0434446e764867d5f762141c2ea
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-11-06 13:14:39 +0900

    delete: 現時点でページ未作成のため、 contact へのリンクをヘッダーから削除

--- src/app/components/organisms/Header.tsx
+++ src/app/components/organisms/Header.tsx
@@ -6,10 +6,9 @@ const Header: React.FC = () => {
 		<div className='bg-black p-4 h-16'>
 			<nav className='flex justify-end gap-5'>
 				<NavButton href='/'>Home</NavButton>
-                <NavButton href='/about'>About</NavButton>
-                <NavButton href='/etrobocon'>ETRobocon</NavButton>
+				<NavButton href='/about'>About</NavButton>
+				<NavButton href='/etrobocon'>ETRobocon</NavButton>
 				<NavButton href='/products'>Products</NavButton>
-				<NavButton href='/contact'>Contact</NavButton>
 			</nav>
 		</div>
 	);
