e555a2f2d5d2b8fcd1a2d47ebd0b59a4306db088
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-11-09 20:55:17 +0900

    add: 作成した CommonButton 用の stories.tsx ファイルを作成

---
+++ src/app/components/atoms/button/CommonButton.stories.tsx
@@ -0,0 +1,69 @@
+import { withActions } from '@storybook/addon-actions/decorator';
+import type { Meta, StoryObj } from '@storybook/react';
+import { expect, userEvent, within } from '@storybook/test';
+import { CommonButton } from './CommonButton';
+
+const meta = {
+	title: 'components/atoms/button/CommonButton',
+	component: CommonButton,
+	parameters: {
+		actions: {
+			handles: ['mouseover', 'click .btn'],
+		},
+	},
+	decorators: [withActions],
+} satisfies Meta<typeof CommonButton>;
+
+export default meta;
+
+type Story = StoryObj<typeof CommonButton>;
+
+export const Default: Story = {
+	args: {
+		buttonAttributes: { type: 'button' },
+		children: 'Common',
+	},
+
+	play: async ({ canvasElement }) => {
+		const canvas = within(canvasElement);
+		// hover時のテスト
+		await userEvent.hover(canvas.getByRole('link'));
+		await expect(canvas.getByRole('link')).toHaveClass('link-hover');
+		// unhover時のテスト
+		await userEvent.unhover(canvas.getByRole('link'));
+		await expect(canvas.getByRole('link')).not.toHaveClass('link-hover');
+	},
+};
+
+export const WithHref: Story = {
+	args: {
+		href: '/',
+		children: 'With Href',
+	},
+
+	play: async ({ canvasElement }) => {
+		const canvas = within(canvasElement);
+		// hover時のテスト
+		await userEvent.hover(canvas.getByRole('link'));
+		await expect(canvas.getByRole('link')).toHaveClass('link-hover');
+		// unhover時のテスト
+		await userEvent.unhover(canvas.getByRole('link'));
+		await expect(canvas.getByRole('link')).not.toHaveClass('link-hover');
+	},
+};
+
+export const OnlyChildren: Story = {
+	args: {
+		children: 'Only Children',
+	},
+
+	play: async ({ canvasElement }) => {
+		const canvas = within(canvasElement);
+		// hover時のテスト
+		await userEvent.hover(canvas.getByRole('link'));
+		await expect(canvas.getByRole('link')).toHaveClass('link-hover');
+		// unhover時のテスト
+		await userEvent.unhover(canvas.getByRole('link'));
+		await expect(canvas.getByRole('link')).not.toHaveClass('link-hover');
+	},
+};
