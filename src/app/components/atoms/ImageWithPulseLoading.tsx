2a736285703367143be34c8b25425da661579433
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-12-16 14:46:45 +0900

    update: 作成した pulse アニメーションの Image コンポーネントを MediaObject で配置

--- src/app/components/atoms/ImageWithPulseLoading.tsx
+++ src/app/components/atoms/ImageWithPulseLoading.tsx
@@ -1,8 +1,8 @@
 'use client';
 
+import Image from 'next/image';
 import { useState } from 'react';
 import type React from 'react';
-import Image from 'next/image';
 
 interface ImageWithPulseLoadingProps {
 	src: string;
