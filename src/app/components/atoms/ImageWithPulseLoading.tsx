1b0d948f4f589e6a799877fb53ff542990f09e82
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-12-16 15:04:45 +0900

    update: 画像の自動サイズ変更により pulse が画像よりも大きくなっていたため、Image コンポーネントに直接記述する形に修正

--- src/app/components/atoms/ImageWithPulseLoading.tsx
+++ src/app/components/atoms/ImageWithPulseLoading.tsx
@@ -17,26 +17,19 @@ export const ImageWithPulseLoading: React.FC<ImageWithPulseLoadingProps> = ({
 	alt,
 	width,
 	height,
-	className = '',
 }) => {
 	const [isLoading, setIsLoading] = useState(true);
 
 	return (
-		<div className='relative'>
-			{isLoading && (
-				<div
-					className={`absolute inset-0 bg-gray-200 animate-pulse rounded-lg ${className}`}
-					style={{ width: `${width}px`, height: `${height}px` }}
-				/>
-			)}
-			<Image
-				src={src}
-				alt={alt}
-				width={width}
-				height={height}
-				className={`${className} ${isLoading ? 'opacity-0' : 'opacity-100'}`}
-				onLoadingComplete={() => setIsLoading(false)}
-			/>
-		</div>
+		<Image
+			src={src}
+			alt={alt}
+			width={width}
+			height={height}
+			className={`transition-opacity duration-300 rounded-lg ${
+				isLoading ? 'opacity-0 bg-gray-200 animate-pulse' : 'opacity-100'
+			}`}
+			onLoadingComplete={() => setIsLoading(false)}
+		/>
 	);
 };
