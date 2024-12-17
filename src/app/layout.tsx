38279f89bfec5c7edf4fdf54b431481aebd6377c
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-12-17 17:40:53 +0900

    update: layout.tsx にメタデータを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,27 @@
+import type { Metadata } from 'next';
 import type React from 'react';
 import 'tailwindcss/tailwind.css';
 import Header from '@/components/organisms/Header';
 
 type LayoutProps = {
 	children?: React.ReactNode;
+};
+
+export const metadata: Metadata = {
+	title: '木村優哉のポートフォリオサイト',
+	description:
+		'木村優哉 (Yuya Kimura) のポートフォリオサイト。宮崎大学大学院に所属している、様々なことに挑戦するWebフロントエンジニア志望の学生です。',
+	openGraph: {
+		title: '木村優哉のポートフォリオサイト',
+		description:
+			'木村優哉 (Yuya Kimura) のポートフォリオサイト。宮崎大学大学院に所属している、様々なことに挑戦するWebフロントエンジニア志望の学生です。',
+	},
+	twitter: {
+		title: '木村優哉のポートフォリオサイト',
+		description:
+			'木村優哉 (Yuya Kimura) のポートフォリオサイト。宮崎大学大学院に所属している、様々なことに挑戦するWebフロントエンジニア志望の学生です。',
+		card: 'summary_large_image',
+	},
 };
 
 const Layout: React.FC<LayoutProps> = ({ children }: LayoutProps) => {
